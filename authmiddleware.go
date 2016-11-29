c51dc415ebb93b5a80600057e5cd539b78b53111
Jesse Millar <hellojessemillar@gmail.com>
2016-11-29 11:49:26 -0700

    Adding some preliminary logging

--- authmiddleware.go
+++ authmiddleware.go
@@ -1,6 +1,7 @@
 package authmiddleware
 
 import (
+	"log"
 	"net/http"
 	"os"
 
@@ -12,11 +13,13 @@ import (
 func Authenticate(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		// First, check if the `LOCAL_ENVIRONMENT` environment variable is set and if it is, return without further checks (circumventing the authentication for local development and Raspberry Pi's)
+		log.Printf("Local check starting")
 		if len(os.Getenv("LOCAL_ENVIRONMENT")) > 0 {
+			log.Printf("Local check finished")
 			next.ServeHTTP(writer, request)
-			return
 		}
 
+		log.Printf("WSO2 check starting")
 		// Next, check for the presence of a JWT token provided by WSO2, if there is one, go through the process of validating it and then return true if it's valid
 		token := request.Header.Get("X-jwt-assertion") // Get the token if it exists
 
